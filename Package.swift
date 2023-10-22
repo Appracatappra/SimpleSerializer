dc1bc68fa095ee5c5c974daa7c99727052268b80
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-10-22 08:17:07 -0500

    Fix versioning
    Fix versioning

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleSerializer",
-    platforms: [.iOS(.v16), .macOS(.v11), .tvOS(.v14), .watchOS(.v7)],
+    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
