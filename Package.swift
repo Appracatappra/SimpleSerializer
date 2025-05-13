c68c9b9f44bf8d17b1cebd56fb478233b6583593
Kevin Mullins <kevin_mullins@appracatappra.com>
2025-05-13 10:14:38 -0500

    Update to v6.1
    Updated to the latest Swift v6.1 compiler.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,11 @@ let package = Package(
         .target(
             name: "SimpleSerializer",
             dependencies: ["LogManager", "SwiftletUtilities"],
-            resources: [.process("Resources")]),
+            resources: [.process("Resources")],
+            swiftSettings: [
+                .swiftLanguageMode(.v6)
+            ]
+        ),
         .testTarget(
             name: "SimpleSerializerTests",
             dependencies: ["SimpleSerializer"]),
